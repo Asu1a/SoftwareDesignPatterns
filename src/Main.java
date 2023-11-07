1bf6483df6dbfd305d715013946385270ec51ff6
Asu1a <abdukarimov.05@gmail.com>
2023-11-07 15:11:43 +0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,54 @@ import FactoryMethodPattern.Mobs;
 import FactoryMethodPattern.MobsFactory;
 import ObserverPattern.Game;
 import SingletonPattern.ServerStatus;
+import StrategyPattern.AxeBehavior;
 import StrategyPattern.SwordBehavior;
 
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
 
+        //STRATEGY PATTERN
+        System.out.println("--------------STRATEGY--------------");
         Characters demon = new Demon(game, "Shoko_O");
         demon.setWeaponBehavior(new SwordBehavior());
+        demon.performFight();
 
         Characters orc = new Orc(game, "UakaBoss");
-        orc.setWeaponBehavior(new SwordBehavior());
-        orc.setWeaponBehavior(new E_Fire(new E_Speed(new SwordBehavior())));
+        orc.setWeaponBehavior(new AxeBehavior());
+        orc.performFight();
 
-        System.out.println("--------------OBSERVER--------------");
+        System.out.println("\n--------------OBSERVER--------------");
         //OBSERVER
         game.getNotificationManager().addSubscriber(demon);
         game.getNotificationManager().addSubscriber(orc);
         game.newUpdate("2 new bosses");
 
-        System.out.println("\n--------------ADAPTER--------------");
 
+        System.out.println("\n--------------ADAPTER--------------");
         //ADAPTER
         demon.setWeaponBehavior(new Weapon_Adapter(new M_Wind()));
         demon.performFight();
-        System.out.println("\n--------------SINGLETON--------------");
 
+
+        System.out.println("\n--------------DECORATOR--------------");
+        //ADAPTER
+        demon.setWeaponBehavior(new E_Fire(new E_Speed(new SwordBehavior())));
+        demon.performFight();
+
+
+        System.out.println("\n--------------SINGLETON--------------");
         //SINGLETON
         ServerStatus serverStatus = ServerStatus.getInstance();
         serverStatus.showStatus();
-        System.out.println("----------------------------");
-
-        orc.performFight();
 
 
+        System.out.println("\n--------------FACTORY--------------");
         //FACTORY
         MobsFactory mobsFactory = new MobsFactory();
         Mobs snake = mobsFactory.createMob("snake");
+        Mobs spider = mobsFactory.createMob("spider");
         snake.attack();
+        spider.attack();
     }
 }
--- src/StrategyPattern/AxeBehavior.java
+++ src/StrategyPattern/AxeBehavior.java
@@ -3,7 +3,7 @@ package StrategyPattern;
 public class AxeBehavior implements WeaponBehavior{
 
     public void weaponAttack() {
-        System.out.println("Chopping with an axe:");
+        System.out.println("Chopping with an axe");
     }
 
 }
